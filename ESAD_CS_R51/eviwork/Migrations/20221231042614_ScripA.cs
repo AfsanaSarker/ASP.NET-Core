3f00dbe1df348aa45e7425022b5a22c885b3d29d
AfsanaSarker <afsanasarker017@gmail.com>
2023-03-11 01:23:30 +0600

    first commit

---
+++ ESAD_CS_R51/eviwork/Migrations/20221231042614_ScripA.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eviwork.Migrations
+{
+    public partial class ScripA : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
